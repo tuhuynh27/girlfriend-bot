53a53707222eafbfed78f54419254bdca2436476
Tu Huynh <huynhminhtufu@gmail.com>
2022-12-02 21:17:18 +0800

    <html> <head><title>301 Moved Permanently</title></head> <body> <center><h1>301 Moved Permanently</h1></center> </body> </html>

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import Layout from './components/layout/Layout.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Layout>
-    <App />
-  </Layout>
+  <App />
 )
